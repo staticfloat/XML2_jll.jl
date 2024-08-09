e3fb19d4263716b3030e87edec7fcc9d18c793ef
Elliot Saba <staticfloat@gmail.com>
2024-08-09 23:14:02 +0000

    XML2 v2.13.2

--- src/XML2_jll.jl
+++ src/XML2_jll.jl
@@ -1,9 +1,5 @@
-# Use baremodule to shave off a few KB from the serialized `.ji` file
-baremodule XML2_jll
-using Base
-using Base: UUID
-import JLLWrappers
+module XML2_jll
+using LazyJLLWrappers
+@generate_jll_from_toml()
+end # module XML2_jll
 
-JLLWrappers.@generate_main_file_header("XML2")
-JLLWrappers.@generate_main_file("XML2", UUID("02c8fc9c-b97f-50b9-bbe4-9be30ff0a78a"))
-end  # module XML2_jll
